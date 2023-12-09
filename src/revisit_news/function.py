46f38fb65d46ede60f949e89316a5f5d3b54fe34
ikenley <ikenley6@gmail.com>
2023-12-09 11:21:10 -0500

    get_historical_articles (#4)

--- src/revisit_news/function.py
+++ src/revisit_news/function.py
@@ -2,6 +2,8 @@ import logging
 import os
 from datetime import date
 import json
+import random
+import re
 from urllib.request import Request, urlopen
 import boto3
 
@@ -30,17 +32,18 @@ def handler(event, context):
     s3_bucket = event_detail["s3_bucket"]
     s3_key = event_detail["s3_key"]
 
+    top_recent_articles = get_articles_from_s3(s3_bucket, s3_key)
+    historical_articles_result = get_historical_articles()
+    send_email(top_recent_articles, historical_articles_result)
+    return "Success"
+
+
+def get_articles_from_s3(s3_bucket, s3_key):
+    """Fetches articles JSON file from a given S3 object"""
     file = get_file_from_s3(s3_bucket, s3_key)
     recent_articles = json.loads(file)
     top_recent_articles = recent_articles[:5]
-
-    # TODO get historical "this day in history" articles
-
-    to_email_addresses = get_to_email_addresses()
-
-    send_email(top_recent_articles, to_email_addresses)
-
-    return "Success"
+    return top_recent_articles
 
 
 def get_file_from_s3(s3_bucket, s3_key):
@@ -49,22 +52,41 @@ def get_file_from_s3(s3_bucket, s3_key):
     return object_content
 
 
-def get_to_email_addresses():
-    response = ssm_client.get_parameter(
-        Name=TO_EMAIL_ADDRESSES_PARAM_NAME,
-        WithDecryption=True
+def get_historical_articles():
+    """Fetches historical articles from random day in history"""
+    s3_key = get_random_historical_file()
+    date_label = get_date_from_s3_key(s3_key)
+    articles = get_articles_from_s3(S3_BUCKET_NAME, s3_key)
+    return {"articles": articles, "date_label": date_label}
+
+
+def get_random_historical_file():
+    response = s3_client.list_objects_v2(
+        Bucket=S3_BUCKET_NAME,
+        Prefix="news/nytimes/mostpopular/emailed/1/2023",
     )
-    to_email_addresses_json = response["Parameter"]["Value"]
-    to_email_addresses = json.loads(to_email_addresses_json)
-    logger.info(f"to_email_addresses={to_email_addresses}")
-    return to_email_addresses
+    response_contents = response["Contents"]
+    random_element = random.choice(response_contents)
+    s3_key = random_element["Key"]
+    logger.info(f"get_random_historical_file:s3_key={s3_key}")
+    return s3_key
 
 
-def send_email(recent_articles, to_email_addresses):
-    html_list = [
-        f"""<li><a href="{a['url']}">{a["title"]}</a></li>""" for a in recent_articles
-    ]
-    text_list = [f"""- {a["title"]}: {a['url']}\n""" for a in recent_articles]
+def get_date_from_s3_key(s3_key):
+    """Get date from S3 key in format news/nytimes/mostpopular/emailed/1/2023/12/08/2023-12-08-news.json"""
+    pattern = r"(\d{4}-\d{2}-\d{2})-news\.json"
+    match_object = re.search(pattern, s3_key)
+    date_label = match_object.group(1)
+    logger.info(f"date_label={date_label}")
+    return date_label
+
+
+def send_email(recent_articles, historical_articles_result):
+    to_email_addresses = get_to_email_addresses()
+
+    recent_message = create_article_list_message(recent_articles)
+    historical_message = create_article_list_message(historical_articles_result["articles"])
+
     ses_client.send_email(
         Destination={
             "ToAddresses": to_email_addresses,
@@ -74,17 +96,21 @@ def send_email(recent_articles, to_email_addresses):
                 "Html": {
                     "Charset": "UTF-8",
                     "Data": f"""The following stories are most emailed NYT artices in the past day:
-                    
                     <ol>
-                    {"".join(html_list)}
+                    {recent_message["html_list"]}
+                    </ol>
+                    The following stories were the most emailed NYT artices on {historical_articles_result["date_label"]}:
+                    <ol>
+                    {historical_message["html_list"]}
                     </ol>
                     """,
                 },
                 "Text": {
                     "Charset": "UTF-8",
                     "Data": f"""The following stories are most emailed NYT artices in the past day:
-                    
-                     {"".join(text_list)}
+                    {recent_message["text_list"]}
+                    The following stories were the most emailed NYT artices on {historical_articles_result["date_label"]}:
+                    {historical_message["text_list"]}
                     """,
                 },
             },
@@ -95,3 +121,23 @@ def send_email(recent_articles, to_email_addresses):
         },
         Source=SES_FROM_EMAIL_ADDRESS,
     )
+
+
+def get_to_email_addresses():
+    """Get email address recipients"""
+    response = ssm_client.get_parameter(
+        Name=TO_EMAIL_ADDRESSES_PARAM_NAME, WithDecryption=True
+    )
+    to_email_addresses_json = response["Parameter"]["Value"]
+    to_email_addresses = json.loads(to_email_addresses_json)
+    logger.info(f"to_email_addresses={to_email_addresses}")
+    return to_email_addresses
+
+
+def create_article_list_message(articles):
+    """Create HTML and plaintext-formatted lists for a given list of articles"""
+    html_list = "".join(
+        [f"""<li><a href="{a['url']}">{a["title"]}</a></li>""" for a in articles]
+    )
+    text_list = "".join([f"""- {a["title"]}: {a['url']}\n""" for a in articles])
+    return {"html_list": html_list, "text_list": text_list}

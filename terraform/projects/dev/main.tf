81dbd34ccbaf915859ce097da5509c51e2f579b3
Ian Kenley <ikenley6@gmail.com>
2026-01-05 06:41:23 -0500

    tf profile

--- terraform/projects/dev/main.tf
+++ terraform/projects/dev/main.tf
@@ -16,8 +16,8 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "terraform-dev"
+  region = "us-east-1"
+  #profile = "terraform-dev"
 }
 
 # ------------------------------------------------------------------------------
